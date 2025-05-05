f921ed361c66e56b9e150497846568c0b6d024cd
ParisAParker <parisaparker2001@gmail.com>
2025-05-05 15:40:22 -0500

    Reorganize files

--- client.py
+++ client.py
@@ -1,59 +1,13 @@
 import pygame
 from network import Network
+from player import Player
+from settings import *
 
 width = 500
 height = 500
 
-WHITE = (255,255,255)
-BLACK = (0,0,0)
-GREEN = (0,255,0)
-RED = (255,0,0)
-
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
-
-client_number = 0
-
-class Player():
-    def __init__(self, x, y, width, height, color):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.color = color
-        self.rect = (x, y, width, height)
-        self.vel = 3
-
-    def draw(self, win):
-        pygame.draw.rect(win, self.color, self.rect)
-
-    def move(self):
-        keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_w]:
-            self.y -= self.vel
-        
-        if keys[pygame.K_s]:
-            self.y += self.vel
-        
-        if keys[pygame.K_a]:
-            self.x -= self.vel
-        
-        if keys[pygame.K_d]:
-            self.x += self.vel
-
-        self.update()
-    
-    def update(self):
-        self.rect = (self.x, self.y, self.width, self.height)
-
-
-def make_pos(tup):
-    return str(tup[0]) + ',' + str(tup[1])
-
-def read_pos(str):
-    str = str.split(",")
-    return int(str[0]), int(str[1])
 
 def redrawWindow(win, player, player2):
     win.fill(WHITE)
@@ -64,18 +18,15 @@ def redrawWindow(win, player, player2):
 def main():
     run = True
     n = Network()
-    startPos = read_pos(n.getPos())
+    p = n.getP()
+    p.color = GREEN
     clock = pygame.time.Clock()
-    p = Player(startPos[0] ,startPos[1], 100, 100, (GREEN))
-    p2 = Player(0, 0, 100, 100, (RED))
 
     while run:
         clock.tick(60)
 
-        p2Pos = read_pos(n.send(make_pos((p.x, p.y))))
-        p2.x = p2Pos[0]
-        p2.y = p2Pos[1]
-        p2.update()
+        p2 = n.send(p)
+        p2.color = RED
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 import socket
 from _thread import *
-import sys
+from player import Player
+from settings import *
 import json
+import pickle
 
 # Load in config.json
 with open("config.json", 'r') as file:
@@ -21,28 +23,19 @@ except socket.error as e:
     print(str(e)) # If an error ocurs, it is converted to a string and printed
 
 # Configuring the socket to listen for incoming connections
-# The argument '2' specifies the maximum number of queued connections
 s.listen(2)
 print("Waiting for a connection, Server Started") # Informing that the server is ready
 
-def make_pos(tup):
-    return str(tup[0]) + ',' + str(tup[1])
-
-def read_pos(str):
-    str = str.split(",")
-    return int(str[0]), int(str[1])
-
-pos = [(0, 0), (100, 100)]
+players = [Player(0, 0, 50, 50, RED), Player(100, 100, 50, 50, GREEN)]
 
 # Function to handle communication with a connected client
 def threaded_client(conn, player):
-    conn.send(str.encode(make_pos(pos[player])))
+    conn.send(pickle.dumps(players[player]))
     reply = ""
     while True: # Infinite loop to keep the connection alive
         try:
-            data = read_pos(conn.recv(2048).decode())
-            pos[player] = data
-            
+            data = pickle.loads(conn.recv(2048))
+            players[player] = data            
 
             # If no data is received, the client has disconnected
             if not data:
@@ -50,14 +43,14 @@ def threaded_client(conn, player):
                 break
             else:
                 if player == 1:
-                    reply = pos[0]
+                    reply = players[0]
                 else:
-                    reply = pos[1]
+                    reply = players[1]
                 # print("Received: ", data)
                 # print("Sending: ", reply)
 
             # Sending the same data back to the client
-            conn.sendall(str.encode(make_pos(reply)))
+            conn.sendall(pickle.dumps(reply))
         except:
             break
 
